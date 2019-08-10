f0bb8c0e173afa3fbb61a88cb49af182faec31f8
frullah <frullah12@gmail.com>
2019-08-11 04:41:23 +0700

    set ID property to pointer

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 // User model
 type User struct {
-	ID       uint64    `json:"id,omitempty"`
+	ID       *uint64   `json:"id,omitempty"`
 	Email    string    `json:"email,omitempty" gorm:"unique_index;size:128;not null"`
 	Username string    `json:"username,omitempty" gorm:"unique_index;size:64;not null"`
 	Password string    `json:"-" gorm:"size:64;not null"`
